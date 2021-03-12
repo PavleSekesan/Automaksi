b56e973bee24fd8d666d4d430e182a5323e28297
PavleSekesan <48137570+PavleSekesan@users.noreply.github.com>
2021-03-12 17:43:18 +0100

    Firestore database now linked with project

--- app/src/main/java/com/example/automaksi/MainActivity.kt
+++ app/src/main/java/com/example/automaksi/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.automaksi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.webkit.WebView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun kupiMaksi(view: View) {
+        val db = Firebase.firestore
+        val docRef = db.collection("Products").document("008lcmgIqmsAyuU1ADF3")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d("kurac", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("kurac", "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("kurac", "get failed with ", exception)
+            }
     }
 }
