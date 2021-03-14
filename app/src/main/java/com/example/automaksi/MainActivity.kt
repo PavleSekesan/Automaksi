4587fe145f0a27e883e2afaa804cd5ec6ece3210
Ognjen <oneskovic0@gmail.com>
2021-03-14 21:57:32 +0100

    Add the barcode scanner: Add the Google ML Kit and the CameraX libraries   Add the barcode scanner activity (layout and kotlin source):     Implement scanned item preview via  web view     Allow the user to accept or decline the scan

--- app/src/main/java/com/example/automaksi/MainActivity.kt
+++ app/src/main/java/com/example/automaksi/MainActivity.kt
@@ -4,16 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.webkit.WebView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+        val intent = Intent(this, BarcodeScannerActivity::class.java)
+        startActivity(intent)
+    }
 
     fun kupiMaksi(view: View) {
         val db = Firebase.firestore
